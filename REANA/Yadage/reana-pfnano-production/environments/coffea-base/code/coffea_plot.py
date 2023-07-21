0bab6a42d7868e7c928c8d689ab82abdfd746387
Xavier Tintin <xavier.tintin@epn.edu.ec>
2023-07-21 17:31:19 +0200

    Workflows/REANA: Add multiple outputs in PFProd

--- REANA/Yadage/reana-pfnano-production/environments/coffea-base/code/coffea_plot.py
+++ REANA/Yadage/reana-pfnano-production/environments/coffea-base/code/coffea_plot.py
@@ -1,13 +1,11 @@
 import awkward as ak
 from coffea.nanoevents import NanoEventsFactory, PFNanoAODSchema
 import matplotlib.pyplot as plt
-import json
 import os
 import sys
 
 
-def save_plot(output_file_path):
-    #fname = "https://raw.githubusercontent.com/cms-dpoa/cat-hackathon/main/data/doubleeg_nanoaod_eg.root"
+def save_plot(output_file_path1, output_file_path2):
     fname = "doubleeg_nanoaod_eg.root"
     events = NanoEventsFactory.from_root(
         fname,
@@ -15,38 +13,28 @@ def save_plot(output_file_path):
         metadata={"dataset": "DoubleEg"},
     ).events()
 
-    # PF candidate collection for jets
-    # print(events.Jet.nConstituents)
-
     # Number of PF candidates, print and save to a text file
     n_pfcands = ak.num(events.PFCands, axis=1)
-
     print(n_pfcands)
     print(events.PFCands.fields)
-
-    output_dir = "output_directory"  # Replace with the desired directory path for output files
-
-    # Save the number of PF candidates to a text file
-    # with open(os.path.join(output_dir, 'PF_n.txt'), 'w') as filehandle:
-    #     json.dump(n_pfcands.tolist(), filehandle)
 
     # Plot PF candidate pt and their number and save the images
     fig, ax = plt.subplots()
     ax.set_yscale('log')
     ax.hist(ak.flatten(events.PFCands.pt), bins=200)
-    # ax.set_title('PF candidate p_t')
-
-    # fig.savefig(os.path.join(output_dir, "PF_pt.png"))
+    ax.set_title('PF candidate p_t')
+    output_directory = os.path.dirname(output_file_path1)
+    png_filename = os.path.join(output_directory, "PF_pt.png")
+    fig.savefig(png_filename)
 
     fig, ax = plt.subplots()
     ax.hist(ak.num(events.PFCands, axis=1), bins=50)
     ax.set_title('n PF candidates')
-
-    output_directory = os.path.dirname(output_file_path)
+    output_directory = os.path.dirname(output_file_path2)
     png_filename = os.path.join(output_directory, "PF_n.png")
     fig.savefig(png_filename)
 
 if __name__ == "__main__":
-    output_file = sys.argv[1]
-    save_plot(output_file)
-    print("Done!")
+    output_file1 = sys.argv[1]
+    output_file2 = sys.argv[2]
+    save_plot(output_file1, output_file2)
